5701fa0a96940fb458891eada79db63a03138777
Maurice Schmidt <mvmogo@gmail.com>
2019-03-23 23:01:47 +0100

    create window

--- source/app.d
+++ source/app.d
@@ -1,6 +1,24 @@
 import std.stdio;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+import bindbc.sdl;
+
+void main() {
+	loadSDL();
+
+	SDL_Window* window = SDL_CreateWindow("Trijam 12 | Black and White", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+	bool closeRequested = false;
+	while (!closeRequested) {
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+				case SDL_QUIT:
+					closeRequested = true;
+					break;
+				default:break;
+			}
+		}
+	}
+	
 }
