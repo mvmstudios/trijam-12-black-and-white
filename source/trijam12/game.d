53a70f39f9f610a4573b7205d74db790784d168e
Maurice Schmidt <mvmogo@gmail.com>
2019-03-23 23:02:01 +0100

    add game class

---
+++ source/trijam12/game.d
@@ -0,0 +1,16 @@
+module trijam12.game;
+
+import bindbc.sdl;
+
+import trijam12.render.renderable;
+
+class Game : Renderable {
+
+    public:
+        override void update(float, float) {}
+
+        override void render(SDL_Renderer* renderer) {
+            
+        }
+
+}
