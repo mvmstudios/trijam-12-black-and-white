ed6ddda36fb667320f6699c722b3e14c3627b3f1
Maurice Schmidt <mvmogo@gmail.com>
2019-03-23 23:49:15 +0100

    instance

--- source/trijam12/game.d
+++ source/trijam12/game.d
@@ -2,15 +2,28 @@ module trijam12.game;
 
 import bindbc.sdl;
 
+import trijam12.entity.player;
 import trijam12.render.renderable;
 
 class Game : Renderable {
+
+    private:
+        static Game _instance;
+
+        Player player
 
     public:
         override void update(float, float) {}
 
         override void render(SDL_Renderer* renderer) {
-            
+
+        }
+
+        static Game instance() {
+            if (!_instance)
+                _instance = new Game;
+
+            return _instance;
         }
 
 }
